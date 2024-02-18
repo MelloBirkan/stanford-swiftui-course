56984783ab3b4730240747b28c1e59753070dc61
Mello Birkan <mgbirkan@icloud.com>
2024-02-18 19:34:40 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Marcello Gonzatto Birkan on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
